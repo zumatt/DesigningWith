b0229cbecb0c92b019e774d8e04d197f0b1a8311
TheGuyWithoutH <ugo.balducci@epfl.ch>
2024-02-15 12:52:16 +0100

    Start testing Icicle

--- designing-with/src/pages/InteractiveFramework.tsx
+++ designing-with/src/pages/InteractiveFramework.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Filter from "../components/Filter";
+import IcicleDiagram from "../components/IcicleDiagram";
 
 const InteractiveFramework = () => {
   return (
     <Layout>
-      <a className="mt-[33px] underline cursor-pointer text-xl">Recommend a tool</a>
+      <a className="mt-[33px] underline cursor-pointer text-xl">
+        Recommend a tool
+      </a>
       <div className="flex flex-row justify-end gap-2">
-        <Filter name = 'Premium' color='border-blue'/>
-        <Filter name = 'Freemium' color='border-purple'/>
-        <Filter name = 'Free' color='border-beige'/>
-        <Filter name = 'Free-Waiting List' color='border-orange'/>
+        <Filter name="Premium" color="border-blue" />
+        <Filter name="Freemium" color="border-purple" />
+        <Filter name="Free" color="border-beige" />
+        <Filter name="Free-Waiting List" color="border-orange" />
       </div>
       <button className="flex w-24">Filter by(+)</button>
       <div className="flex flex-row">
-        <div className="flex flex-col">
-        </div>
+        <IcicleDiagram />
       </div>
     </Layout>
-    );
+  );
 };
 
 export default InteractiveFramework;
