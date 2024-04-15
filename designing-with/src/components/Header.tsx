fbd1fd2983cc287a1e591590d84cefc640e56b5a
SpiderMan6699 <gregmalyk@gmail.com>
2024-04-15 15:49:07 +0200

    bigger menu

--- designing-with/src/components/Header.tsx
+++ designing-with/src/components/Header.tsx
@@ -16,10 +16,10 @@ const Header = ({
     <div className="h-full w-full">
       <div className="flex flex-row gap-2">
         <a
-          className="sm:flex md:hidden justify-center bg-white rounded-xl px-3 z-10"
+          className="sm:flex md:hidden  justify-center bg-white rounded-xl px-3 z-10"
           href="/"
         >
-          <p>DW</p>
+          <p className="text-[2.4vh]">DW</p>
         </a>
         <a
           className="hidden md:flex justify-center bg-white rounded-xl px-3 z-10"
@@ -31,28 +31,28 @@ const Header = ({
           onClick={onClick}
           className="flex justify-start bg-white rounded-xl px-3 z-10"
         >
-          <p style={ {transition: "all 300ms", transform: menuIsOpen ? "rotate(45deg)" : ""}}>+</p>
+          <p className="text-[2.4vh]" style={ {transition: "all 300ms", transform: menuIsOpen ? "rotate(45deg)" : ""}}>+</p>
         </button>
         {menuIsOpen ? <Results /> : null}
         {!menuIsOpen && pathname === "/about" && (
-          <p className="w-fit bg-purple rounded-xl px-3">About </p>
+          <p className="w-fit text-[2.4vh] bg-purple rounded-xl px-3">About </p>
         )}
         {!menuIsOpen && pathname === "/ai-for-designers" && (
-          <p className="w-fit bg-blue rounded-xl px-3">AI for Designers </p>
+          <p className="w-fit text-[2.4vh] bg-blue rounded-xl px-3">AI for Designers </p>
         )}
         {!menuIsOpen && pathname === "/interactive-framework" && (
-          <p className="w-fit bg-grey rounded-xl px-3">
+          <p className="w-fit text-[2.4vh] bg-grey rounded-xl px-3">
             Interactive Framework{" "}
           </p>
         )}
         {!menuIsOpen && pathname === "/didactic-guidelines" && (
-          <p className="w-fit bg-green rounded-xl px-3">Didactic Guidelines </p>
+          <p className="w-fit text-[2.4vh] bg-green rounded-xl px-3">Didactic Guidelines </p>
         )}
         {!menuIsOpen && pathname === "/resources" && (
-          <p className="w-fit bg-orange rounded-xl px-3">Resources </p>
+          <p className="w-fit text-[2.4vh] bg-orange rounded-xl px-3">Resources </p>
         )}
         {!menuIsOpen && pathname === "/" && (
-          <p className="w-fit bg-beige rounded-xl px-3">Let’s talk! </p>
+          <p className="w-fit text-[2.4vh] bg-beige rounded-xl px-3">Let’s talk! </p>
         )}
       </div>
       {menuIsOpen ? <BlurredBack /> : null}
@@ -71,25 +71,25 @@ const BlurredBack = () => (
 const Results = () => (
   <div className="flex flex-col h-4 gap-2 z-10">
     <a className="w-fit bg-purple rounded-xl px-3" href="/about">
-      <p>About</p>
+      <p className="text-[2.4vh]">About</p>
     </a>
     <a className="w-fit bg-blue rounded-xl px-3" href="/ai-for-designers">
-      <p>AI for Designers</p>
+      <p className="text-[2.4vh]">AI for Designers</p>
     </a>
     <a className="w-fit bg-grey rounded-xl px-3" href="/interactive-framework">
-      <p>Interactive Framework</p>
+      <p className="text-[2.4vh]">Interactive Framework</p>
     </a>
     <a className="w-fit bg-green rounded-xl px-3" href="/didactic-guidelines">
-      <p>Didactic Guidelines</p>
+      <p className="text-[2.4vh]">Didactic Guidelines</p>
     </a>
     <a className="w-fit bg-orange rounded-xl px-3" href="/resources">
-      <p>Resources</p>
+      <p className="text-[2.4vh]">Resources</p>
     </a>
     <a
       className="w-fit bg-beige rounded-xl px-3"
       href="mailto:designingwithai@supsi.ch?subject=Let’s talk! - Designing With AI Website"
     >
-      <p>Let’s talk!</p>
+      <p className="text-[2.4vh]">Let’s talk!</p>
     </a>
   </div>
 );
