2835bac279cd48b5725e3c7c1eecf01bb5a1e0de
Matteo Subet <hi@zumat.ch>
2024-06-06 14:51:29 +0200

    Update DesignProcessTab.tsx

--- designing-with/src/components/AIForDesignersTabs/DesignProcessTab.tsx
+++ designing-with/src/components/AIForDesignersTabs/DesignProcessTab.tsx
@@ -29,7 +29,7 @@ const DesignProcessTab: TabElement = {
       </div>
       <div className="bg-[#D9D9D9] bg-opacity-50 shadow-[inset_0_0_20px_5px_rgba(1,1,1,0)] shadow-white px-4 py-6 rounded-b-2xl">
           <DesignPhaseSvg />
-          <p className="mt-[33px] mb-[25px]">
+          <p className="text-[12px] md:text-[15px]">
           © Botta, M., Autuori, A., Subet, M., Terenghi, G. (2024). Design Process Model for Education.
           </p>
       </div>
